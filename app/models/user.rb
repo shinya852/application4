162198754b5ec7244702e7791306e5719ac37a7c
shinya852 <shinya.june@gmail.com>
2021-10-09 13:12:46 +0000

    ボタン修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,7 @@ class User < ApplicationRecord
 
   has_many :favorites, dependent: :destroy#いいね機能
 
-    def already_favorited?(book)
-      self.favorites.exists?(book_id: book.id)
-    end
+
   # コメント機能
   has_many :book_comments, dependent: :destroy
 
@@ -24,8 +22,18 @@ class User < ApplicationRecord
   #フォローしている人全員を持ってくる
   has_many :followings, through: :relationships, source: :follower
 
+
   #フォローされる側
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
   #フォローしてくれている人を全部持ってくる
   has_many :followers, through: :reverse_of_relationships, source: :following
+
+  def already_favorited?(book)
+      self.favorites.exists?(book_id: book.id)
+  end
+
+  def already_following?(user)
+      self.followings.exists?(id: user.id)
+  end
+
 end
